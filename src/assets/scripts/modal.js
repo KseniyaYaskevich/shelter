5909a45682f1d405a6b776ac16284bed71e40ac2
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-05-14 11:44:01 +0300

    feat: add basic scripts

---
+++ src/assets/scripts/modal.js
@@ -0,0 +1,34 @@
+const pageBody = document.querySelector('.page__body');
+const cardsContainer = document.querySelector('.our-friends__cards')
+const modalWrapper = pageBody.querySelector('.modal-wrapper');
+const modal = pageBody.querySelector('.modal');
+const buttonModal = pageBody.querySelector('.modal__button');
+
+const openModal = () => {
+  pageBody.classList.add('page__body--lock');
+  modalWrapper.classList.add('modal-wrapper--show');
+  modal.classList.add('modal--show');
+};
+
+const closeModal = () => {
+  pageBody.classList.remove('page__body--lock');
+  modalWrapper.classList.remove('modal-wrapper--show');
+  modal.classList.remove('modal--show');
+};
+
+const onCardsContainerClick = (evt) => {
+  if (evt.target.closest('.our-friends__card')) {
+    openModal();
+  }
+};
+
+document.onclick = function (evt) {
+  if (modal.classList.contains('modal--show')) {
+    if (evt.target.classList.contains('modal-wrapper--show')) {
+      closeModal();
+    }
+  }
+};
+
+cardsContainer.addEventListener('click', onCardsContainerClick);
+buttonModal.addEventListener('click', closeModal);
