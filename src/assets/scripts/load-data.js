c02bd53815159213143df9370b575b676b60b95d
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-05-26 15:26:43 +0300

    feat: shuffle given number of cards before render

--- src/assets/scripts/load-data.js
+++ src/assets/scripts/load-data.js
@@ -1,10 +1,18 @@
 import getData from './get-data';
 import renderItems from './render-items';
+import mixItem from './mix-item';
 
-const loadData = () => {
-    getData().then((data) => {
-        renderItems(data);
-    });
+const loadData = (cardsNumber) => {
+  getData().then((data) => {
+
+    const getCard = () => {
+      return mixItem(data);
+    };
+
+    const cards = new Array(cardsNumber).fill().map(getCard);
+
+    renderItems(cards);
+  });
 };
 
 export default loadData;
