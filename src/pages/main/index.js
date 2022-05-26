32b4409e640a8e1b30eb5bfa509cbb2b7e8aee74
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-05-26 15:28:43 +0300

    feat: create and pass a variable

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,3 +2,8 @@ import 'assets/styles/style.scss';
 import 'assets/scripts/index.js';
 import './styles/main.scss';
 import './scripts/script.js';
+import load from 'assets/scripts/load-data';
+
+const CARDS_NUMBER = 8;
+
+load(CARDS_NUMBER);
--- src/pages/pets/index.js
+++ src/pages/pets/index.js
@@ -2,3 +2,8 @@ import 'assets/styles/style.scss';
 import 'assets/scripts/index.js';
 import './styles/main.scss';
 import './scripts/script.js';
+import load from 'assets/scripts/load-data';
+
+const CARDS_NUMBER = 48;
+
+load(CARDS_NUMBER);
