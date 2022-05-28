7c4c169e5c5c07fd480eb7cf6106cae073317889
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-05-28 14:10:51 +0300

    fix: swiper parameter

--- src/pages/pets/scripts/swiper.js
+++ src/pages/pets/scripts/swiper.js
@@ -25,20 +25,24 @@ const swiperPets = new Swiper('.swiper-pets', {
 
   breakpoints: {
     320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
       grid: {
         rows: 3,
-        columns: 1,
       },
       spaceBetween: 30,
     },
     768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
       grid: {
         rows: 3,
-        columns: 2,
       },
       spaceBetween: 30,
     },
     1280: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
       grid: {
         rows: 2,
         columns: 4,
